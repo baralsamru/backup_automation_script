f06a2bc61cc2d0dce1d3df78ffe64e9555474630
Samrachana Baral <134517111+baralsamru@users.noreply.github.com>
2025-12-07 11:12:26 +0545

    Modify backup script to preserve file permissions
    Updated backup script to preserve permissions during compression.

--- backup.sh
+++ backup.sh
@@ -8,21 +8,18 @@ DATE=$(date +%F)
 BACKUP_NAME="Documents_backup_$DATE.tar.gz"
 FULL_PATH="$BACKUP_DIR/$BACKUP_NAME"
 
+# Fix permissions before backup
+chmod -R u+rwX,go-rwx "$SOURCE_DIR"
+
 # Create backup folder
 mkdir -p "$BACKUP_DIR"
 
-# Create compressed backup
-tar -czf "$FULL_PATH" "$SOURCE_DIR"
+# Create compressed backup while preserving permissions
+tar -czpf "$FULL_PATH" "$SOURCE_DIR"
 
-# Check if  the bacup is successful
+# Check if the backup is successful
 if [ $? -eq 0 ]; then
-    echo " Backup successful!"
+    echo "Backup successful!"
 else
-    echo "Oops.  Backup failed."
+    echo "Oops. Backup failed."
 fi
-
-
-
-
-
-
